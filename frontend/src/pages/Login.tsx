ad3a9bf66a5cb54b6e4dcbe5b0bda913300129f0
Aditi <staraditi719@gmail.com>
2026-05-15 09:53:35 +0530

    Update API URL to use Nginx port 80

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 
-const API = 'http://65.2.166.74:8000'
+const API = 'http://65.2.166.74'
 export default function Login() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
